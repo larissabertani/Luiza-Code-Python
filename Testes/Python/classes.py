c9d86aa657500be82ae497308d8a7c2899875fbe
LarissaBertani <larissabertani1997@hotmail.com>
2022-09-08 22:07:39 -0300

    Exercício Classes (tipos)

--- Testes/Python/classes.py
+++ Testes/Python/classes.py
@@ -1,34 +1,3 @@
-
-# class Doguinhos:
-#     raca = 'shitzu'
-#     def __init__(self, nome, idade):
-#         self.nome = nome
-#         self. idade = idade
-
-# cachorro1 = Doguinhos('Noobinho', '10')
-# cachorro2 = Doguinhos('Lulucrey', '2')
-
-# print(cachorro1.nome)
-# print(cachorro2.idade)
-
-
-class Netflix:
-    def __init__(self, nome, genero, temporadas):
-        self.nome = nome
-        self.genero = genero
-        self.temporadas = temporadas
-    
-    def obter_temporadas_serie(self):
-        return f"A série {self.nome} possui {self.temporadas} temporadas disponíveis para assistir."
-    
-serie = Netflix('Peaky Blinders', 'Drama', '5')
-
-qtd_temporadas = serie.obter_temporadas_serie()
-print('Temporadas:', qtd_temporadas)
-
-# Temporadas: A série Peaky Blinders possui 5 temporadas disponíveis para assistir.
-
-
 class Esportes:
     def __init__ (self, nome, local, tipo, participantes):
         self.nome = nome
